86e6d50590d7b3a998bf13f755f8bb600d62bbbb
AakarshAgrawal <aakarsh9101@gmail.com>
2025-08-06 08:43:22 +0000

    Variables in JavaScript

--- JS_basics/variables.js
+++ JS_basics/variables.js
@@ -1,3 +1,10 @@
-let a = 1;
-a = 2;
-console.log(a);
+const AccountID = "123456";
+let AccountEmail = "Aakarsh@email.com";
+var AccountPass = "121212";
+AccontCity = "Delhi";
+//AccountID = "33443";
+/* we cannot re-assign or change value assigned in const, while let and Var can be reassigned */
+AccountEmail = "New@email.com";
+AccountPass = "212121";
+AccountCity ="Mumbai";
+console.table([AccountID, AccountEmail, AccountPass, AccountCity]);
