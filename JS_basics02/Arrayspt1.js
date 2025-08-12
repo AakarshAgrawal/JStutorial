334db1478181bd3cce572078c36c8f88b9985786
AakarshAgrawal <aakarsh9101@gmail.com>
2025-08-12 06:43:43 +0000

    Basics of Arrays in Javascript

---
+++ JS_basics02/Arrayspt1.js
@@ -0,0 +1,5 @@
+// An array is a special object in JavaScript used to store ordered collections of values.
+// Values can be of any type (numbers, strings, objects, functions, even other arrays).
+
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(myArr)
