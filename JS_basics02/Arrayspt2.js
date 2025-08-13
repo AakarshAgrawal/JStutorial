32906572260d7a3a2a21da43db8f19acb64f3b5a
AakarshAgrawal <aakarsh9101@gmail.com>
2025-08-13 17:33:34 +0000

    Basics in Array

---
+++ JS_basics02/Arrayspt2.js
@@ -0,0 +1,5 @@
+const marvelheros = ["ironMan", "spiderMan", "Thor", "BlackPanther"];
+
+const DCheros = ["Batman", "Superman", "Joker"];
+marvelheros.push(DCheros);  // we can push anything in the array, for ex: number, string, character, Another Array also
+console.log (marvelheros); 
