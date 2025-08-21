eb971cd0c5d2551b6701e56fba41d9ae4c6b210d
AakarshAgrawal <aakarsh9101@gmail.com>
2025-08-21 07:00:18 +0000

    Completed JS Practice Project 01

---
+++ practiseProject01/script.js
@@ -0,0 +1,34 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+
+
+button.forEach(function(button){
+ button.addEventListener("click", function(e){
+   console.log(e.target);
+   if(e.target.id === "red"){
+     body.style.backgroundColor = e.target.id;
+   }
+   else if (e.target.id === "green"){
+    body.style.backgroundColor = e.target.id;
+   }
+    else if (e.target.id === "blue"){
+        body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === "yellow"){
+        body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === "purple"){
+        body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === "white"){
+        body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === "teal"){
+        body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === "LightSalmon"){
+        body.style.backgroundColor = e.target.id;
+    }
+ });
+});
