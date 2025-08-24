5a0f0ed022f965853ffc431e220ebdf6d2b41dd2
AakarshAgrawal <aakarsh9101@gmail.com>
2025-08-24 07:48:15 +0000

    practiceProject02 initialised

---
+++ practiceProject04/script.js
@@ -0,0 +1,5 @@
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+const userInput = document.getElementById("guessField");
+const submit = document.getElementById ("submit");
